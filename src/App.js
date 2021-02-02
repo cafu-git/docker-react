50c47537ece41bf40901ac4114a31b18f60e466d
Cristian Garcia <cafucg@gmail.com>
2021-02-02 11:52:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world  <code>THIS IS IS MY FIRST REACT APP</code>
         </p>
         <a
           className="App-link"
